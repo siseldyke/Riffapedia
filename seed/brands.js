2638739ac8a3bc8f6adc8d0c34a70141246fc42f
siseldyke <siseldyke@gmail.com>
2024-10-18 12:22:45 -0400

    details for seeds added, assorted downloads to project

--- seed/brands.js
+++ seed/brands.js
@@ -4,27 +4,33 @@
 
 const brands =[
     {
-        name: '',
-        founder: '',
-        yearFounded: 1979
+        name: 'Fender',
+        founder: 'leo fender',
+        yearFounded: 1946
       
     },
     {
-        name: '',
-        founder: '',
-        yearFounded: 1979
+        name: 'Gibson',
+        founder: 'orville gibson',
+        yearFounded: 1894
       
     },
     {
-        name: '',
-        founder: '',
-        yearFounded: 1979
+        name: 'Ibanez',
+        founder: 'hoshino gakki',
+        yearFounded: 1957
       
     },
     {
-        name: '',
-        founder: '',
-        yearFounded: 1979
+        name: 'PRS',
+        founder: 'paul reed smith',
+        yearFounded: 1985
+      
+    },
+    {
+        name: 'Gretsch',
+        founder: 'friedrich gretsch',
+        yearFounded: 1883
       
     },
 ]
--- seed/guitars.js
+++ seed/guitars.js
@@ -4,12 +4,93 @@
 
 const guitars =[
     {
-        modelName: '',
-        brand: '',
+        modelName: 'Les Paul Junior Silver Mist',
+        brand: 'gibson',
+        type:'electric',
+        averagePrice: 1900,
+        description: 'etc',
+        image: 'etc'
+        
+    },
+    {
+        modelName: 'Gretsch Guitars G6136-55 Vintage Select Edition 55 Falcon Hollowbody',
+        brand: 'gretsch',
+        type:'electric',
+        averagePrice: 4000,
+        description: 'etc',
+        image: 'etc'
+        
+    },
+    {
+        modelName: 'PRS SE Santana',
+        brand: 'PRS',
+        type:'electric',
+        averagePrice: 3000,
+        description: 'etc',
+        image: 'etc'
+        
+    },
+    {
+        modelName: 'Fender Stratocaster Olympic White',
+        brand: 'Fender',
+        type:'electric',
+        averagePrice: 1199,
+        description: 'etc',
+        image: 'etc'
+        
+    },
+    {
+        modelName: 'Frankenstrat',
+        brand: 'custom',
         type:'electric',
         averagePrice: 0,
-        description: '',
-        image: ''
+        description: 'etc',
+        image: 'etc'
+        
+    },
+    {
+        modelName: '1960 Gibson Les Paul Standard',
+        brand: 'gibson',
+        type:'electric',
+        averagePrice: 13000,
+        description: 'etc',
+        image: 'etc'
+        
+    },
+    {
+        modelName: 'Gibson ES-335',
+        brand: 'gibson',
+        type:'electric',
+        averagePrice: 3500,
+        description: 'etc',
+        image: 'etc'
+        
+    },
+    {
+        modelName: 'Ibanez YY10',
+        brand: 'ibanez',
+        type:'electric',
+        averagePrice: 1099,
+        description: 'etc',
+        image: 'etc'
+        
+    },
+    {
+        modelName: 'Gretsch G6120T-HR',
+        brand: 'gretsch',
+        type:'electric',
+        averagePrice: 3600,
+        description: 'etc',
+        image: 'etc'
+        
+    },
+    {
+        modelName: 'Fender Telecaster',
+        brand: 'fender',
+        type:'electric',
+        averagePrice: 1200,
+        description: 'etc',
+        image: 'etc'
         
     },
 ]
